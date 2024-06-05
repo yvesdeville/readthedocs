01da0a797e7d32792a6edfd8972409006e54ba3e
yannrichet <richetyann@gmail.com>
2024-06-05 16:06:19 +0200

    fix lib.loc to use related dev

--- docs/source/make_functions_examples.sh
+++ docs/source/make_functions_examples.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # previously geenrate functions content using https://docs.ropensci.org/r2readthedocs/
+# > sudo apt install python3-sphinx-code-include python3-sphinx-rst-builder python3-myst-parser python3-sphinx-rtd-theme
 # R> remotes::install_github("ropenscilabs/r2readthedocs")
 # R> r2readthedocs::r2readthedocs("../rlibkriging")
 # cp -r ../rlibkriging/docs/functions/*.md docs/source/functions/.
@@ -10,7 +11,7 @@
 for f in `ls functions`; do
     echo $f
     
-    echo "library(rlibkriging)" > /tmp/$f.R
+    echo "library(rlibkriging, lib.loc='../../../libKriging/bindings/R/Rlibs')" > /tmp/$f.R
     echo "png('functions/examples/$f.png')"        >> /tmp/$f.R
     cat functions/$f | sed '1,/Examples/d' | sed '1,/```r/d'  | sed '/```/,$d' >> /tmp/$f.R
     echo "w<-dev.off()"            >> /tmp/$f.R
